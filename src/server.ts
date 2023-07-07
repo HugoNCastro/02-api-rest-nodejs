cf64a2ce7ef9567aa549fa61e8f340542a75b12a
HugoNCastro <hugonaaman@gmail.com>
2023-07-07 14:18:40 -0300

    Realiza query e faz chamada na rota

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return transaction
+})
+
+app.get('/get-transactions', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
