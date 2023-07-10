71ed7523a248afc007cdba26e7dff078f91d7d4d
HugoNCastro <hugonaaman@gmail.com>
2023-07-10 11:40:45 -0300

    Cria rota para criar transactions usando plugin do fastify

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return transaction
-})
-
-app.get('/get-transactions', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
